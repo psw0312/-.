82473d5b89f7dc7459be0c7ddd4f3be7ce451e30
psw0312 <81516083+psw0312@users.noreply.github.com>
2021-03-28 21:08:08 +0800

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Calculator
+    {
+        double a, b;
+
+        public double A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+        public double B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+        public char c;
+        public void add()
+        {
+            Console.WriteLine("{0}+{1}={2}", a,b,a + b);
+        }
+        public void less()
+        {
+            Console.WriteLine("{0}-{1}={2}", a, b, a - b);
+        }
+        public void multiply()
+        {
+            Console.WriteLine("{0}*{1}={2}", a, b, a * b);
+        }
+        public void divided()
+        {
+            if (b != 0)
+                Console.WriteLine("{0}/{1}={2}", a, b, a / b);
+            else
+                Console.WriteLine("除数不能为0");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c1 = new Calculator();
+            do
+            {
+                try
+                {
+                    c1.A = double.Parse(Console.ReadLine());
+                    c1.B = double.Parse(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("请输入数字:");
+                }
+            }
+            while (true);
+                c1.c = Convert.ToChar(Console.ReadLine());
+            switch (c1.c)
+                {
+                    case '+':
+                        c1.add();
+                        break;
+                    case '-':
+                        c1.less();
+                        break;
+                    case '*':
+                        c1.multiply();
+                        break;
+                    case '/':
+                        c1.divided();
+                        break;
+                }
+            
+        }
+    }
+}
